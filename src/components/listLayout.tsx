87cd5d81a6cdb98b65940def9792fc06b313640e
KimYuKyung <kyg0954@naver.com>
2023-09-08 22:50:31 +0900

    feat: listLayout 컴포넌트에 button 관련 내용 추가

--- src/components/listLayout.tsx
+++ src/components/listLayout.tsx
@@ -1,13 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { styled } from 'styled-components';
+
+interface ButtonInfoProps {
+  button1: {
+    value: string;
+    onClick: () => void;
+  };
+  button2?: {
+    value: string;
+    onClick: () => void;
+  };
+}
 
 interface ListLayoutProps {
   title: string;
   src?: string;
+  buttons?: boolean;
+  buttonInfo: ButtonInfoProps;
   children?: React.ReactNode;
 }
 
-function ListLayout_({ title, src, children }: ListLayoutProps) {
+function ListLayout_({
+  title,
+  src,
+  buttons = false,
+  buttonInfo,
+  children,
+}: ListLayoutProps) {
   return (
     <ListLayout>
       <Title>
@@ -15,9 +35,53 @@ function ListLayout_({ title, src, children }: ListLayoutProps) {
         <h2>{title}</h2>
       </Title>
       <List>{children}</List>
+      <Buttons>
+        {buttons ? (
+          <>
+            <Button onClick={buttonInfo.button1.onClick}>
+              {buttonInfo.button1.value}
+            </Button>
+            <Button>{buttonInfo.button2?.value}</Button>
+          </>
+        ) : (
+          <>
+            <Button onClick={buttonInfo.button1.onClick}>
+              {buttonInfo.button1.value}
+            </Button>
+          </>
+        )}
+      </Buttons>
     </ListLayout>
   );
 }
+
+const Buttons = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 80px;
+  padding: 20px 59px;
+  color: #ffffff;
+  font-size: 3.2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  border-radius: 25px;
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  -moz-transition: background, 0.3s;
+  -o-transition: background, 0.3s;
+  -webkit-transition: background, 0.3s;
+  transition: background, 0.3s;
+
+  &:hover {
+    border-radius: 25px;
+    background: #a7c2e4;
+    box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
 
 const ListLayout = styled.div`
   display: flex;
@@ -25,9 +89,11 @@ const ListLayout = styled.div`
   justify-content: center;
   width: 100%;
   height: calc(100vh - 88px);
-  min-height: 700px;
+  min-height: 800px;
   max-height: 1000px;
-  gap: 20px;
+  gap: 27px;
+  padding-top: 3vh;
+  padding-bottom: 5vh;
 `;
 
 const Title = styled.div`
@@ -54,7 +120,7 @@ const List = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 80%;
+  height: 100%;
   background: rgba(255, 255, 255, 0.4);
   box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
   margin-bottom: auto;
