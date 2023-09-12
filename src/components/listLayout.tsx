5365da555284f02ab0850b53b6b58ff69d63d3ad
KimYuKyung <kyg0954@naver.com>
2023-09-12 19:30:56 +0900

    feat: listLayout 컴포넌트에서 버튼 2개일 경우 고려해서 추가

--- src/components/listLayout.tsx
+++ src/components/listLayout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { styled } from 'styled-components';
 
@@ -34,14 +33,14 @@ function ListLayout_({
         <img src={src} />
         <h2>{title}</h2>
       </Title>
-      <List>{children}</List>
+      <List $title={title}>{children}</List>
       <Buttons>
         {buttons ? (
           <>
-            <Button onClick={buttonInfo.button1.onClick}>
+            <TwoButton onClick={buttonInfo.button1.onClick}>
               {buttonInfo.button1.value}
-            </Button>
-            <Button>{buttonInfo.button2?.value}</Button>
+            </TwoButton>
+            <TwoButton>{buttonInfo.button2?.value}</TwoButton>
           </>
         ) : (
           <>
@@ -54,38 +53,6 @@ function ListLayout_({
     </ListLayout>
   );
 }
-
-const Buttons = styled.div`
-  display: flex;
-`;
-
-const Button = styled.button`
-  width: 100%;
-  height: 80px;
-  padding: 20px 59px;
-  color: #ffffff;
-  font-size: 3.2rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  border-radius: 25px;
-  background: rgba(255, 255, 255, 0.4);
-  box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
-  -moz-transition: background, 0.3s;
-  -o-transition: background, 0.3s;
-  -webkit-transition: background, 0.3s;
-  transition: background, 0.3s;
-
-  &:focus {
-    background-color: #a7c2e4;
-  }
-
-  &:hover {
-    border-radius: 25px;
-    background: #a7c2e4;
-    box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
-  }
-`;
 
 const ListLayout = styled.div`
   display: flex;
@@ -120,7 +87,7 @@ const Title = styled.div`
   }
 `;
 
-const List = styled.div`
+const List = styled.div<{ $title: string }>`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -128,10 +95,69 @@ const List = styled.div`
   background: rgba(255, 255, 255, 0.4);
   box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
   margin-bottom: auto;
-  padding: 37px 33px;
+  padding: ${(props) =>
+    props.$title === '결과 조회' ? '37px 21px' : '37px 33px'};
   position: relative;
   overflow: hidden;
   border-radius: 25px;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 29px;
+`;
+
+const TwoButton = styled.button`
+  width: 274px;
+  height: 49px;
+  color: #ffffff;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  border-radius: 25px;
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  -moz-transition: background, 0.3s;
+  -o-transition: background, 0.3s;
+  -webkit-transition: background, 0.3s;
+  transition: background, 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    border-radius: 25px;
+    background: #a7c2e4;
+    box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 80px;
+  padding: 20px 59px;
+  color: #ffffff;
+  font-size: 3.2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  border-radius: 25px;
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  -moz-transition: background, 0.3s;
+  -o-transition: background, 0.3s;
+  -webkit-transition: background, 0.3s;
+  transition: background, 0.3s;
+  cursor: pointer;
+
+  &:focus {
+    background-color: #a7c2e4;
+  }
+
+  &:hover {
+    border-radius: 25px;
+    background: #a7c2e4;
+    box-shadow: 0px 4px 25px 0px rgba(0, 0, 0, 0.25);
+  }
 `;
 
 export default ListLayout_;
