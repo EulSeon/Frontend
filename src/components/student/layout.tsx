c5c506333ac0390c8baf5e6f95d0620f3de4ef39
KimYuKyung <kyg0954@naver.com>
2023-10-03 23:50:54 +0900

    feat: student 페이지 layout 컴포넌트 제작

---
+++ src/components/student/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+interface StudentLayoutProps {
+  children?: React.ReactNode;
+}
+
+function StudentLayout_({ children }: StudentLayoutProps) {
+  return <StudentLayout>{children}</StudentLayout>;
+}
+
+const StudentLayout = styled.div`
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  background-color: #ececec;
+`;
+
+export default StudentLayout_;
