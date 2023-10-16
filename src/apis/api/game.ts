f2b5ba4d506f996d56f3710819832d30d733a043
KimYuKyung <kyg0954@naver.com>
2023-10-16 13:14:32 +0900

    fix: 게임방 비밀번호 가져오는 api url 변경

--- src/apis/api/game.ts
+++ src/apis/api/game.ts
@@ -3,7 +3,7 @@ import { defaultInstance } from '../utils/instance';
 // 게임방 비밀번호 가져오기
 export const getGameRoomPassword = async () => {
   try {
-    const { data, status } = await defaultInstance.get(`/roomCode`);
+    const { data, status } = await defaultInstance.get(`/room`);
     return { data, status };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
