4a5c521249ce2fb02f5862c89b6fd08ec72c61eb
KimYuKyung <kyg0954@naver.com>
2023-10-16 18:07:25 +0900

    feat: 게임방 업데이트하는 api 적용

--- src/apis/api/game.ts
+++ src/apis/api/game.ts
@@ -3,10 +3,36 @@ import { defaultInstance } from '../utils/instance';
 // 게임방 비밀번호 가져오기
 export const getGameRoomPassword = async () => {
   try {
-    const { data, status } = await defaultInstance.get(`/room`);
+    const { data, status } = await defaultInstance.post(`/rooms`);
     return { data, status };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
-    return { message: e.response.data.error, status: e.response.status };
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
+  }
+};
+
+interface RoomInfo {
+  round_num: number;
+  time_limit: number;
+  seed: number;
+}
+
+// 게임방 정보 업데이트하기
+export const updateRoomInfo = async (roomPW: string, roomInfo: RoomInfo) => {
+  try {
+    const { data, status } = await defaultInstance.put(
+      `/rooms/${roomPW}`,
+      roomInfo
+    );
+    return { data, status };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
   }
 };
