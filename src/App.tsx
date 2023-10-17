a7021a55526ccdc3a71cbeb50111d97593fe7ffe
KimYuKyung <kyg0954@naver.com>
2023-10-17 16:36:34 +0900

    feat: 라우터 일단 임시로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Main_ from '@pages/Main';
+import Main from '@pages/Main';
 import WaitingRoomLayout from '@pages/WaitingRoom';
 import GameResult from '@pages/GameResult';
+import Buy from '@pages/student/Buy';
+import Sell from '@pages/student/Sell';
+import Wallet from '@pages/student/Wallet';
+import StudentMain from '@pages/student/Main';
 
 function App() {
   return (
@@ -12,9 +16,15 @@ function App() {
       <GlobalStyle />
       <Router>
         <Routes>
-          <Route path="/" element={<Main_ />}></Route>
+          <Route path="/" element={<Main />}></Route>
           <Route path="/room/wait" element={<WaitingRoomLayout />}></Route>
           <Route path="/room/1/result" element={<GameResult />}></Route>
+
+          {/* 학생들 */}
+          <Route path="/student" element={<StudentMain />}></Route>
+          <Route path="/student/wallet" element={<Wallet />}></Route>
+          <Route path="/student/buy" element={<Buy />}></Route>
+          <Route path="/student/sell" element={<Sell />}></Route>
         </Routes>
       </Router>
     </>
