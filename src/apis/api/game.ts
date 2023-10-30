b30e2417ffd56b69dd22846468ae1d1c7a6678b3
KimYuKyung <kyg0954@naver.com>
2023-10-30 21:44:29 +0900

    feat: 게임방 나가기, 다음 라운드로 넘어가기, 게임 결과 조회, 저장 API 생성

--- src/apis/api/game.ts
+++ src/apis/api/game.ts
@@ -61,3 +61,73 @@ export const participateGameRoom = async (
     };
   }
 };
+
+// 게임방 나가기
+export const leaveGameRoom = async (roomPW: string) => {
+  try {
+    const { data, status } = await defaultInstance.delete(`/users/${roomPW}`);
+    return { data, status };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
+  }
+};
+
+// 게임방 다음 라운드로 넘어가기
+export const goNextRound = async (roomPW: string) => {
+  try {
+    const { data, status } = await defaultInstance.put(`/rooms/${roomPW}/next`);
+    return { data, status };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
+  }
+};
+
+// 게임 결과 조회하기
+export const checkGameResult = async (
+  roomPW: string,
+  round: number,
+  opt: number
+) => {
+  try {
+    const { data, status } = await defaultInstance.get(
+      `/rooms/${roomPW}/result?round=${round}&opt=${opt}`
+    );
+    return { data, status };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
+  }
+};
+
+interface SaveGameResult {
+  round_num: number;
+}
+
+// 게임 결과 저장하기
+export const saveGameResult = async (roomPW: string, round: SaveGameResult) => {
+  try {
+    console.log(round);
+    const { data, status } = await defaultInstance.post(
+      `/rooms/${roomPW}/result`,
+      round
+    );
+    return { data, status };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return {
+      status: e.response.status,
+      error: e.response.data.error,
+    };
+  }
+};
