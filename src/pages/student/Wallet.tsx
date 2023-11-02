8bee343ff55b0bcd8ac56d0b88c2d2b322bc0f02
KimYuKyung <kyg0954@naver.com>
2023-11-02 11:32:55 +0900

    fix: 게임 끝난 후 메인화면으로 이동할 때 스크롤 불가능하도록 설정되어있는 문제 해결

--- src/pages/student/Wallet.tsx
+++ src/pages/student/Wallet.tsx
@@ -95,6 +95,12 @@ function Game() {
       document.body.style.cssText = '';
       window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
     }
+
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
   }, [modalState.visible, finish]);
 
   return (
